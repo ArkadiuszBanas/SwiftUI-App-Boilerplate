b26c50cf74498f04f4def9f1b6fdba110952b308
Arkadiusz Banaś <mail@arkadiuszbanas.com>
2023-10-21 12:56:14 +0200

    Add empty project

---
+++ SwiftUI-App-Boilerplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-App-Boilerplate
+//
+//  Created by Arek on 21/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-App-Boilerplate/SwiftUI_App_BoilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_App_BoilerplateApp.swift
+//  SwiftUI-App-Boilerplate
+//
+//  Created by Arek on 21/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_App_BoilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
