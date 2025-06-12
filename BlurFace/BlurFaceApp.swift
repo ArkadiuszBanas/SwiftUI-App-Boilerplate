6653e86745d8651ed40ad9892764e5256d7ca6d9
Arkadiusz Banaś <mail@arkadiuszbanas.com>
2025-06-12 15:15:40 +0200

    Configure revenue cat

--- BlurFace/BlurFaceApp.swift
+++ BlurFace/BlurFaceApp.swift
@@ -6,15 +6,32 @@
 //
 
 import SwiftUI
+import RevenueCat
 import OnBoardingKit
 
 @main
 struct BlurFaceApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
             AppCoordinator()
                 .presentOnBoarding(BlurFaceOnboarding())
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+
+        Purchases.logLevel = .debug
+        Purchases.configure(withAPIKey: "appl_fXLxCBJIoPFRrODisNYlNEnyXcE")
+
+        return true
     }
 }
